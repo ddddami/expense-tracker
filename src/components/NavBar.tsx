be48bb26872f858c4f53a8d0a168ae20d9c42e0f
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-09-12 15:54:45 +0100

    Fix: link styles in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
+import { NavLink } from "react-router-dom";
 import UserContext from "../contexts/UserContext";
+import { logout } from "../services/auth-service";
 
 const NavBar = () => {
   const { user } = useContext(UserContext);
@@ -10,14 +12,27 @@ const NavBar = () => {
         <ul className="nav-list d-inline-block me-auto mb-2 mb-lg-0 list-inline">
           <li className="list-inline-item px-1">
             {user ? (
-              <a className="nav-link">Logout</a>
+              <NavLink
+                to="#"
+                className="nav-link"
+                onClick={() => {
+                  logout();
+                  window.location.href = "/login";
+                }}
+              >
+                Logout
+              </NavLink>
             ) : (
-              <a className="nav-link">Register</a>
+              <NavLink to="/register" className="nav-link">
+                Register
+              </NavLink>
             )}
           </li>
           {!user && (
             <li className="list-inline-item px-1">
-              <a className="list-inline-item nav-link">Login</a>
+              <NavLink to="/login" className="list-inline-item nav-link">
+                Login
+              </NavLink>
             </li>
           )}
         </ul>
